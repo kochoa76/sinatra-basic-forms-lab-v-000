b65d5bd81ce748fa1c7173c72b97dcd58278cbfa
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-16 23:05:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,22 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get "/" do
+  get '/' do
     erb :index
   end
 
-  get "/new" do
+  get '/new' do
     erb :create_puppy
   end
 
-  post "/" do
-    @name = params[:name]
-    @breed =params[:breed]
-    @age= params[:age]
-
-
+  post '/puppy' do
+    @name = params['name']
+    @age = params['age']
+    @breed = params['breed']
     erb :display_puppy
   end
 
 
 end
+
